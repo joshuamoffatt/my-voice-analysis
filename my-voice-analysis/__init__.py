c1a669f9dc6335ed915d26161e9b0b607a31f1ed
joshuamoffatt <33057920+joshuamoffatt@users.noreply.github.com>
2021-08-12 16:35:23 +0100

    Removed a print that seemed to always print "[]".

--- my-voice-analysis/__init__.py
+++ my-voice-analysis/__init__.py
@@ -253,7 +253,7 @@ def mysptotal(m,p, debug=True):
     path=p+"/"
     try:
         objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
-        print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
+        #print (objects[0]) # This will print the info from the sound object, and objects[0] is a parselmouth.Sound object
         z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
         z2=z1.strip().split()
         z3=np.array(z2)
